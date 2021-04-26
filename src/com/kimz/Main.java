5e7ba86843b8f15139c35d44f3e9090f016dddd5
Kimutai <kimutai0003@gmail.com>
2021-04-26 13:27:16 +0300

    Theatre class now complete

--- src/com/kimz/Main.java
+++ src/com/kimz/Main.java
@@ -3,6 +3,9 @@ package com.kimz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 Theatre theatre = new Theatre("Fox", 8, 12);
+	 theatre.getSeats();
+
+
     }
 }
