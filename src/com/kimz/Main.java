2ae24c60f2aaa71dea4441fc21d93aca8f97e11b
Kimutai <kimutai0003@gmail.com>
2021-04-27 00:39:45 +0300

    final stages

--- src/com/kimz/Main.java
+++ src/com/kimz/Main.java
@@ -1,22 +1,34 @@
 package com.kimz;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	 Theatre theatre = new Theatre("Fox", 8, 12);
-	 //theatre.getSeats();
-        if (theatre.reserveSeat("C03")){
-            System.out.println("seat reserved " + "please pay!");
-        }else{
-            System.out.println("seat already reserved");
-        }
 
-        if (theatre.reserveSeat("C03")){
-            System.out.println("seat reserved " + "please pay!");
-        }else{
-            System.out.println("seat already reserved");
-        }
+	 List<Theatre.Seat> seats = new ArrayList<>(theatre.getSeats());
 
+
+        printList(seats);
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
 
     }
+
+    public static void printList(List<Theatre.Seat> list){
+        for (Theatre.Seat seat: list){
+            System.out.print(" " + seat.getSeatNumber() + " $" + seat.getPrice());
+        }
+        System.out.println();
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    }
+
+
 }
